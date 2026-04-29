f5ff760b3dec9ca5d2f83b7f76db4e0d2248d113
luca-cat <lucaranieridinzey@gmail.com>
2026-04-29 11:07:04 +0100

    added support for .oga file extension and condensed response requests into an anki function

--- field_converter.py
+++ field_converter.py
@@ -2,50 +2,54 @@ import requests
 
 def convert_audio(value):
     if not value:
-        return value.strip()
-    return value.replace(".ogg", ".mp3").replace(".opus", ".mp3")
+        return value
+    #if value is empty it returns the original empty value
+    if ".mp3" in value:
+        return value
+    #if .mp3 already present it returns the original value
+    print(f"converting {value}")
+    return value.replace(".ogg", ".mp3").replace(".opus", ".mp3").replace(".oga", ".mp3")
+    #any other cases will cause this return function to run
+
+def anki(action, params):
+    response = requests.post(url, json={
+        "action": action,
+        "version":6,
+        "params": params
+    })
+    return response.json()
 
 url = "http://localhost:8765"
 
 deck_name = "sentencemining"
 
+note_ids = anki("findNotes", {"query": f"deck:{deck_name}"})["result"]
 
-response = requests.post(url, json={
-    "action": "findNotes",
-    "version": 6,
-    "params": {"query": f"deck:{deck_name}"}
-})
+choice = input("would you like to proceed? ").lower()
+if choice == "yes":
 
-note_ids = response.json()["result"]
+    for note_id in note_ids:
+        notes = anki("notesInfo", {"notes": [note_id]})
+        fields = notes["result"][0]["fields"]
+        sentence_audio = fields["SentenceAudio"]["value"]
+        word_audio = fields["WordAudio"]["value"]
+        
+        new_sentence_audio = convert_audio(sentence_audio)
+        new_word_audio = convert_audio(word_audio)
 
-for note_id in note_ids:
-    response_note = requests.post(url, json={
-    "action": "notesInfo",
-    "version":6,
-    "params":{
-        "notes":[note_id]
-    }
-    })
-    fields = response_note.json()["result"][0]["fields"]
-    sentence_audio = fields["SentenceAudio"]["value"]
-    word_audio = fields["WordAudio"]["value"]
-    
-    sentence_audio = convert_audio(sentence_audio)
-    word_audio = convert_audio(word_audio)
+        if word_audio != new_word_audio or sentence_audio != new_sentence_audio:
 
-    print(sentence_audio)
-    print(word_audio)
-    
-    response_note = requests.post(url, json={
-    "action": "updateNoteFields",
-    "version": 6,
-    "params": {
-        "note": {
-            "id": note_id,
-            "fields": {
-                "WordAudio":word_audio,
-                "SentenceAudio":sentence_audio
-            }
-        }
-    }
-})
+            anki("updateNoteFields", {
+                "note": {
+                    "id": note_id,
+                    "fields": {
+                        "WordAudio":new_word_audio,
+                        "SentenceAudio":new_sentence_audio
+                    }
+                }
+            })
+
+elif choice == "no":
+    pass
+else:
+    print("not a choice")
